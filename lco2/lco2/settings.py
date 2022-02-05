cda9b47abce80d808f1618c37358f788faca6f33
mohamed493 <62177483+mohamed493@users.noreply.github.com>
2022-02-05 02:10:01 +0200

    update the settings

--- lco2/lco2/settings.py
+++ lco2/lco2/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7dc&2)97-i=9_5lx--=su2=@x_h7xlgps=fw7ootcyqd_l)-4d'
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
